c96230c3e1d5c7cecd410611955ebfae41de9764
meso1007 <94849818+meso1007@users.noreply.github.com>
2023-07-25 15:42:45 +0900

    Create index.js
    マウスオーバーイベント

---
+++ index.js
@@ -0,0 +1,24 @@
+function changeColor1() {
+    document.getElementById("js-top").style.backgroundColor = "#BAD3FF"
+}
+function revertColor1() {
+    document.getElementById("js-top").style.backgroundColor = null
+}
+function changeColor2() {
+    document.getElementById("js-pro").style.backgroundColor = "#DCC2FF"
+}
+function revertColor2() {
+    document.getElementById("js-pro").style.backgroundColor = null
+}
+function changeColor3() {
+    document.getElementById("js-che").style.backgroundColor = "#CBFFD3"
+}
+function revertColor3() {
+    document.getElementById("js-che").style.backgroundColor = null
+}
+function changeColor4() {
+    document.getElementById("js-pls").style.backgroundColor = "#2CEEFF"
+}
+function revertColor4() {
+    document.getElementById("js-pls").style.backgroundColor = null
+}
